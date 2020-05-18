9da8375eecccf3eafd3ddd50980133d0cfcbbb6a
Philip Markowski <philipmarkowski642@gmail.com>
2020-05-18 14:16:27 -0400

    Add instructional text

--- src/app/components/StateEditor.js
+++ src/app/components/StateEditor.js
@@ -3,21 +3,38 @@ import { connect } from 'react-redux'
 import { deleteSelectedObject } from '../reducers/dungeonReducer'
 
 let StateEditor = ({ dispatch, selectedObjectId }) => {
-    return (
-        <React.Fragment>
+    if (selectedObjectId) {
+        return <React.Fragment>
             {selectedObjectId &&
                 <div className="card bg-dark text-light border-secondary">
                     <div className="card-header border-secondary">
                         <h5>Actions</h5>
                     </div>
                     <div className="card-body">
-
-                        <button className="btn btn-outline-danger" onClick={() => dispatch(deleteSelectedObject())}>Delete Object</button>
+                        <button
+                            className="btn btn-outline-danger"
+                            onClick={() => dispatch(deleteSelectedObject())}>
+                                Delete Object
+                        </button>
                     </div>
                 </div>
             }
         </React.Fragment>
-    )
+    }
+    else {
+        return <div className="card bg-dark text-light border-secondary">
+            <div className="card-header border-secondary">
+                <h5>Instructions</h5>
+            </div>
+            <div className="card-body">
+                <p><i>Right click</i> to pan the view.</p>
+                <p><i>Scroll</i> to zoom in and out.</p>
+                <p><i>Left click and drag</i> to create new spaces with the New Space tool.</p>
+                <p><i>Left click</i> to select spaces with the Select tool.</p>
+                <p><i>Delete</i> will delete the currently selected space.</p>
+            </div>
+        </div>
+    }
 }
 
 const mapStateToProps = state => ({
