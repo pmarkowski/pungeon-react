289b940df6caba853593fac1595f9e404161becc
Philip Markowski <philipmarkowski642@gmail.com>
2020-05-18 10:02:02 -0400

    Move app to a subfolder

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './app/App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import store from './store.js'
+import store from './app/store.js'
 
 ReactDOM.render(
   <React.StrictMode>
