61ab3ff0b5d6ad6d2e0d29d2cbe8671228f62d40
Philip Markowski <philipmarkowski642@gmail.com>
2020-06-13 09:04:18 -0400

    Move toolbar buttons to a component

--- src/app/components/Toolbar.js
+++ src/app/components/Toolbar.js
@@ -1,28 +1,28 @@
 import React from "react"
 import { connect } from 'react-redux'
 import { selectTool } from '../reducers/dungeonReducer'
+import ToolbarButton from "./ToolbarButton"
 
 let Toolbar = ({ dispatch, selectedTool }) => {
+    const dispatchSelectedTool = (toolId) => dispatch(selectTool(toolId))
+
     return (
         <div className="btn-group" role="group">
-            <button
-                className={"btn btn-secondary" +
-                    (selectedTool === 'Select' ? ' active' : '')}
-                onClick={() => dispatch(selectTool('Select'))}>
-                    Select
-            </button>
-            <button
-                className={"btn btn-secondary" +
-                    (selectedTool === 'NewSpace' ? ' active' : '')}
-                onClick={() => dispatch(selectTool('NewSpace'))}>
-                    New Space
-            </button>
-            <button
-                className={"btn btn-secondary" +
-                    (selectedTool === 'NewWall' ? ' active' : '')}
-                onClick={() => dispatch(selectTool('NewWall'))}>
-                    New Wall
-            </button>
+            <ToolbarButton
+                toolName='Select'
+                toolId='Select'
+                selectedTool={selectedTool}
+                onClick={dispatchSelectedTool} />
+            <ToolbarButton
+                toolName='New Space'
+                toolId='NewSpace'
+                selectedTool={selectedTool}
+                onClick={dispatchSelectedTool} />
+            <ToolbarButton
+                toolName='New Wall'
+                toolId='NewWall'
+                selectedTool={selectedTool}
+                onClick={dispatchSelectedTool} />
         </div>
     )
 }
