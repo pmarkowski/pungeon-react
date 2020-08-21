59cbdb2512629cc78fd4aa65873a61e9b94db98c
Philip Markowski <philipmarkowski642@gmail.com>
2020-08-21 10:47:05 -0400

    Clean up DungeonRenderer.js

--- src/app/rendering/DungeonRenderer.js
+++ src/app/rendering/DungeonRenderer.js
@@ -2,7 +2,6 @@ import * as PIXI from 'pixi.js';
 import { selectObject, setMouseDungeonPosition } from "../reducers/dungeonReducer";
 import store from '../store.js';
 import { GRID_TILE_SIZE } from '../utils/constants';
-import DUNGEON_OBJECT_TYPE from '../utils/dungeonObjectTypes';
 import * as ToolRouter from '../tools/ToolRouter';
 import * as RenderRouter from './RenderRouter'
 
@@ -20,7 +19,7 @@ export const render = (app, graphics) => {
     drawGrid(graphics, state.dungeon.size.width, state.dungeon.size.height);
 
     if (app.renderer.plugins.interaction.mouseOverRenderer) {
-        drawMouseCursor(state, graphics);
+        ToolRouter.renderTool(state, graphics);
 
         let mousePosition = app.renderer.plugins.interaction.mouse.getLocalPosition(app.stage);
         store.dispatch(setMouseDungeonPosition(mousePosition.x, mousePosition.y));
@@ -56,32 +55,13 @@ const drawDungeonObjects = (container, state) => {
         if (graphics.id) {
             let object = objectIdMap[graphics.id];
             if (object) {
-                switch (object.type) {
-                    case DUNGEON_OBJECT_TYPE.SPACE:
-                        drawSpace(graphics, object, state);
-                        break;
-                    case DUNGEON_OBJECT_TYPE.WALL:
-                        drawWall(graphics, object, state);
-                        break;
-                    case DUNGEON_OBJECT_TYPE.DOOR:
-                        drawDoor(graphics, object, state);
-                        break;
-                    case DUNGEON_OBJECT_TYPE.LABEL:
-                        drawLabel(graphics, object, state);
-                        break;
-                    default:
-                        break;
-                }
+                RenderRouter.renderObject(graphics, object, state.selectedObject === graphics.id)
             }
             else {
                 container.removeChild(graphics);
             }
         }
     });
-}
-
-const drawMouseCursor = (state, graphics) => {
-    ToolRouter.renderTool(state, graphics);
 }
 
 const drawGrid = (graphics, gridWidth, gridHeight) => {
@@ -94,21 +74,4 @@ const drawGrid = (graphics, gridWidth, gridHeight) => {
         graphics.moveTo(0, j * GRID_TILE_SIZE);
         graphics.lineTo((gridWidth - 1) * GRID_TILE_SIZE, j * GRID_TILE_SIZE);
     }
-}
-
-
-const drawWall = (graphics, wall, state) => {
-    RenderRouter.renderObject(graphics, wall, state.selectedObject === graphics.id)
-}
-
-const drawSpace = (graphics, space, state) => {
-    RenderRouter.renderObject(graphics, space, state.selectedObject === graphics.id)
-}
-
-const drawDoor = (graphics, door, state) => {
-    RenderRouter.renderObject(graphics, door, state.selectedObject === graphics.id);
-}
-
-const drawLabel = (graphics, label, state) => {
-    RenderRouter.renderObject(graphics, label, state.selectedObject === graphics.id)
 }
