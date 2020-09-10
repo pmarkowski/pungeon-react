7c47da3c14f5dabd65f51411e71b0200c0f99b8f
Philip Markowski <philipmarkowski642@gmail.com>
2020-09-10 12:33:24 -0400

    Draw an outline of the token you will add

--- src/app/tools/AddTokenTool.js
+++ src/app/tools/AddTokenTool.js
@@ -1,7 +1,29 @@
+import * as PIXI from 'pixi.js'
+import { GRID_TILE_SIZE } from '../utils/constants';
+
 export default class AddTokenTool {
+    // TODO: Make this configurable based on the currently selected token or something along those lines
+    height = 2;
+    width = 1;
+
     onMouseUp(store) {
+        // Add the sprite/token object to the dungeon
     }
 
     renderTool(state, graphics) {
+        // preview the sprite in the current location
+        let mousePoint = state.editor.mouse.dungeonPosition;
+        let snappedX, snappedY;
+
+        // snap to nearest grid point
+        // for now for simplicity let's say top left
+        snappedX = Math.floor(mousePoint.x / GRID_TILE_SIZE) * GRID_TILE_SIZE;
+        snappedY = Math.floor(mousePoint.y / GRID_TILE_SIZE) * GRID_TILE_SIZE;
+
+        // draw a hover rect
+        graphics.beginFill(0, 0);
+        graphics.lineStyle(1, 0xfffd00);
+        graphics.drawRect(snappedX, snappedY, this.width * GRID_TILE_SIZE, this.height * GRID_TILE_SIZE);
+        graphics.endFill();
     }
 }
