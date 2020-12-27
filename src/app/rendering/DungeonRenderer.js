f41dc1043b8875f21b21ab33c994e0e69acd01f1
Philip Markowski <philipmarkowski642@gmail.com>
2020-12-27 11:49:14 -0500

    Remove unnecessary subtraction when drawing grid

--- src/app/rendering/DungeonRenderer.js
+++ src/app/rendering/DungeonRenderer.js
@@ -76,12 +76,12 @@ const drawGrid = (graphics, gridWidth, gridHeight) => {
     graphics.lineStyle(1, 0x444444, 1, 0.5);
     for (let i = 0; i <= gridWidth; i++) {
         graphics.moveTo(i * GRID_TILE_SIZE, 0);
-        graphics.lineTo(i * GRID_TILE_SIZE, gridHeight * GRID_TILE_SIZE - 1);
+        graphics.lineTo(i * GRID_TILE_SIZE, gridHeight * GRID_TILE_SIZE);
     }
 
     for (let j = 0; j <= gridHeight; j++) {
         graphics.moveTo(0, j * GRID_TILE_SIZE);
-        graphics.lineTo(gridWidth * GRID_TILE_SIZE - 1, j * GRID_TILE_SIZE);
+        graphics.lineTo(gridWidth * GRID_TILE_SIZE, j * GRID_TILE_SIZE);
     }
 }
 
