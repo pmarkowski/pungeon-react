82bbab286118ec450bc8063a5d026c72a68454ba
Philip Markowski <philipmarkowski642@gmail.com>
2021-02-16 13:35:08 -0500

    Extract clipboard functions to new module

---
+++ src/app/utils/clipboard.js
@@ -0,0 +1,37 @@
+import { v4 as uuid } from "uuid";
+import { position, translate } from "../dungeonObjects/DungeonObjectOperations";
+import { addObject } from "../reducers/dungeonActions";
+import { addObjectsToClipboard, selectObjects } from "../reducers/editorActions";
+import { GRID_TILE_SIZE } from "./constants";
+import copyObject from "./copyObject";
+
+export const pasteFromClipboard = (state, store) => {
+    let copiedObjects = state.editor.clipboard.map(object => {
+        object.id = uuid();
+        return copyObject(object);
+    });
+
+    let mousePosition = {
+        x: Math.floor(state.editor.mouse.currentPosition.x / GRID_TILE_SIZE),
+        y: Math.floor(state.editor.mouse.currentPosition.y / GRID_TILE_SIZE)
+    };
+    let firstPositionOfSelection = position(state.editor.clipboard[0]);
+    let clipboardTranslation = {
+        x: mousePosition.x - firstPositionOfSelection.x,
+        y: mousePosition.y - firstPositionOfSelection.y
+    };
+
+    // TODO: Make this an array of objects so that it's an atomic action and can be undone
+    copiedObjects.forEach(object => {
+        translate(object, clipboardTranslation.x, clipboardTranslation.y);
+        store.dispatch(addObject(object));
+    });
+    store.dispatch(selectObjects(copiedObjects.map(object => object.id)));
+}
+
+export const copyToClipboard = (store, state) => {
+    store.dispatch(addObjectsToClipboard(
+        state.dungeon.objects
+            .filter(object => state.editor.selectedObjectIds.includes(object.id))
+            .map(copyObject)));
+}
