36ff77b8ec46283d24d0a6bfe10fe3d111507387
Philip Markowski <philipmarkowski642@gmail.com>
2021-02-18 10:21:15 -0500

    Fix bug in ToolbarButton hover styles

--- src/app/components/ToolbarButton.js
+++ src/app/components/ToolbarButton.js
@@ -2,8 +2,11 @@ import React from "react"
 
 const ToolbarButton = ({className, toolName, toolId, selectedTool, onClick}) =>
     <button
-        className={"px-4 py-2 bg-gray-200 hover:bg-gray-300  dark:bg-gray-700 dark:hover:bg-gray-600" +
-            (selectedTool === toolId ? 'bg-blue-300 hover:bg-blue-300 dark:bg-blue-900 dark:hover:bg-blue-900' : ' ') + className}
+        className={"px-4 py-2 " +
+            (selectedTool === toolId ?
+                'bg-blue-300 hover:bg-blue-300 dark:bg-blue-900 dark:hover:bg-blue-900' :
+                'bg-gray-200 hover:bg-gray-300  dark:bg-gray-700 dark:hover:bg-gray-600')
+            + className}
         onClick={() => onClick(toolId)}>
             {toolName}
     </button>
